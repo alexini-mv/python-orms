0f0c582574f5fc668b35d751531e2bf237df9bbf
Alejandro Martínez-Valencia <vaf.ss4@gmail.com>
2022-10-12 03:21:38 -0500

    Construimos el primer modelo

--- 1-sqlalchemy/base.py
+++ 1-sqlalchemy/base.py
@@ -7,8 +7,11 @@ from sqlalchemy.orm import sessionmaker
 
 # Abrimos una conexión a la base de datos. En este caso SQLite.
 # En este caso, si no existe, crea el archivo de la base de datos.
+
+# Definimos los tres objetos que exportaremos. El primero es el objeto 
+# que creará la conexión a la base de datos.
 engine = create_engine('sqlite:///BaseDeDatos.db')
 # Iniciamos una sesión
 Session = sessionmaker(bind=engine)
-# Declaramos el objeto que construira el schema
+# Declaramos el objeto que construirá el schema de la Base de Datos
 Base = declarative_base()
---
+++ 1-sqlalchemy/models.py
@@ -0,0 +1,29 @@
+# Declaramos los modelos a partir de los cuales se instanciarán los objetos en la BD.
+
+# Importamos de SQLAlchemy los tipos que mapearan a la tabla. Recordemos que cada
+# atributo será una columna dentro de la tabla. Y cada columna tendrá un tipo de 
+# dato especifico. Además puede existir relaciones entre las diferentes tablas.
+from sqlalchemy import Column, String, Integer
+from .base import Base
+
+
+class User(Base):
+    # Es obligatorio que todo modelo tenga un __tablename__
+    __tablename__ = 'users'
+    
+    # Declaramos las columnas de la tabla.
+    id = Column(String, primary_key=True)
+    name = Column(String(30))
+    last_name = Column(String(30))
+    age = Column(Integer)
+
+    def __init__(self, uid, name, last_name, age) -> None:
+        self.uid = uid 
+        self.name = name 
+        self.last_name = last_name
+        self.age = age
+
+    # Este método no es obligatorio, pero es bueno tenerlo para saber como es representado
+    # el objeto que estamos declarando
+    def __repr__(self) -> str:
+        return f"User(id={self.uid}, name={self.name}, last_name={self.last_name}, age={self.age})"
